66bd4fe0a94c8f7c52fa083dcb7c343fb42bc6b7
Supercomp <123808637+supercompanyet@users.noreply.github.com>
2023-06-05 11:43:14 -0700

    Update supabase.ts

--- app/services/supabase.ts
+++ app/services/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://dlzrbdvxxsfduuhsysiu.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+const supabaseUrl = {'https://dlzrbdvxxsfduuhsysiu.supabase.co'}
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
